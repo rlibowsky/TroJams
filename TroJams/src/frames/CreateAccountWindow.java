4c8fe1381602fe30cb09cd0f40679dd49a0cfd09
Ruth Libowsky <Ruth@guest-wireless-upc-1606-10-120-105-010.usc.edu>
2016-11-15 22:32:08 -0800

    create account updated

--- TroJams/src/frames/CreateAccountWindow.java
+++ TroJams/src/frames/CreateAccountWindow.java
@@ -1,37 +1,111 @@
 package frames;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import logic.User;
+import resources.AppearanceConstants;
+import resources.AppearanceSettings;
 
 public class CreateAccountWindow extends JFrame {
 
+	private static final long serialVersionUID = 1L;
 	private User newUser;
 	private LoginScreenWindow loginScreenWindow;
 	
-	private JTextField emailTextField, usernameTextField, passwordTextField, ageTextField, firstNameTextField, lastNameTextField;
-	
+	private JTextField usernameTextField, passwordTextField, firstNameTextField, lastNameTextField;
+	private JLabel infoLabel, instructionsLabel, imageLabel;
+	private JButton submitButton;
+	private ImageIcon silhouette;
 	
 	
 	
 	public CreateAccountWindow(User newUser, LoginScreenWindow loginScreenWindow){
+		super("Create Account");
 		this.newUser = newUser;
 		this.loginScreenWindow = loginScreenWindow;
+		initializeComponents();
+		createGUI();
+		addListeners();
 	}
 	
 	
+	private void addListeners() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 	private void initializeComponents(){
-		emailTextField = new JTextField();
 		usernameTextField= new JTextField();
 		passwordTextField= new JTextField();
-		ageTextField= new JTextField();
 		firstNameTextField= new JTextField();
 		lastNameTextField= new JTextField();
+		infoLabel = new JLabel("New User");
+		instructionsLabel = new JLabel("Please enter your information");
+		imageLabel = new JLabel();
+        
+		submitButton = new JButton("Submit");
 	}
 	
 	private void createGUI(){
+		setSize(700, 600);
+		setLayout(new BorderLayout());
+		JPanel infoPanel = new JPanel();
+		JPanel textFieldPanel = new JPanel();
+		JPanel bottomPanel = new JPanel();
 		
+		//set font for the labels
+		AppearanceSettings.setFont(AppearanceConstants.fontMedium, instructionsLabel);
+		AppearanceSettings.setFont(AppearanceConstants.fontLarge, infoLabel);
+		
+		//set alignment
+		AppearanceSettings.setTextAlignment(instructionsLabel, infoLabel);
+		
+		//set sizes
+		//AppearanceSettings.setSize(400, 60, usernameTextField, passwordTextField, firstNameTextField, lastNameTextField);
+		
+		infoPanel.setLayout(new BorderLayout());
+		infoPanel.add(infoLabel, BorderLayout.NORTH);
+		infoPanel.add(instructionsLabel, BorderLayout.SOUTH);
+		
+		textFieldPanel.setLayout(new GridLayout(4,1));
+		firstNameTextField.setPreferredSize(new Dimension(10, 60));
+		lastNameTextField.setPreferredSize(new Dimension(10, 60));
+		usernameTextField.setPreferredSize(new Dimension(10, 60));
+		passwordTextField.setPreferredSize(new Dimension(10, 60));
+		textFieldPanel.add(firstNameTextField);
+		textFieldPanel.add(lastNameTextField);
+		textFieldPanel.add(usernameTextField);
+		textFieldPanel.add(passwordTextField);
+		
+		bottomPanel.setLayout(new BorderLayout());
+		imageLabel.setPreferredSize(new Dimension(200,200));
+		Image image = new ImageIcon("images/silhouette.png").getImage();
+		silhouette = new ImageIcon(image.getScaledInstance(200, 200, java.awt.Image.SCALE_SMOOTH));
+		imageLabel.setIcon(silhouette);
+		bottomPanel.add(imageLabel, BorderLayout.WEST);
+		bottomPanel.add(submitButton, BorderLayout.EAST);
+		
+		add(infoPanel, BorderLayout.NORTH);
+		add(textFieldPanel, BorderLayout.CENTER);
+		add(bottomPanel, BorderLayout.SOUTH);
+		
+	}
+	
+	public static void main(String [] args) {
+		System.out.println("test!");
+		CreateAccountWindow caw = new CreateAccountWindow(new User("test", "test"), new LoginScreenWindow());
+		caw.setVisible(true);
 	}
 	
 	
