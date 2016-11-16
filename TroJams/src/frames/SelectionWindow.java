5f3ba4358c7b58bbab90808938309ed446739166
Catherine Chung <cathercc@usc.edu>
2016-11-16 01:48:29 -0800

    added jank selection window, will fix to make more pretty later

--- TroJams/src/frames/SelectionWindow.java
+++ TroJams/src/frames/SelectionWindow.java
@@ -1,7 +1,7 @@
 package frames;
 
 import java.awt.BorderLayout;
-import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -11,22 +11,24 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 
 /*
  * SELECTION WINDOW: THIS IS OUR MAIN WINDOW, WHERE WE CHOOSE WHICH PARTY TO JOIN/CREATE A PARTY
  */
 
-import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.SpringLayout;
 
 import logic.User;
 import resources.AppearanceConstants;
 import resources.AppearanceSettings;
 
-public class SelectionWindow extends JFrame{
+public class SelectionWindow extends JFrame {
 
 	private JPanel mainPanel;
 	
@@ -87,11 +89,8 @@ public class SelectionWindow extends JFrame{
 		JPanel topPanel = new JPanel();
 		
 		topPanel.setSize(AppearanceConstants.GUI_WIDTH, (AppearanceConstants.GUI_HEIGHT)/4);
-		//topPanel.setSize(1280, 200);
 
-		//topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
 		AppearanceSettings.setFont(AppearanceConstants.fontMedium, createAPartyButton);
-		//AppearanceSettings.addGlue(topPanel, BoxLayout.PAGE_AXIS, false, createAPartyButton);
 		topPanel.add(createAPartyButton);
 		topPanel.add(Box.createHorizontalGlue());
 		return topPanel;
@@ -99,10 +98,32 @@ public class SelectionWindow extends JFrame{
 	
 	// creates the bottom panel, which houses a jscrollpane
 	private JPanel createBottomPanel() {
-		JPanel bottomPanel = new JPanel();
-		JScrollPane songsScrollPane = new JScrollPane();
-		Container contentPane = this.getContentPane();
+		JPanel bottomPanel = new JPanel(new SpringLayout());
+		JScrollPane scroll = new JScrollPane();
+		
+		SpringLayout layout = new SpringLayout();
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(layout);
+		bottomPanel.setLayout(new BorderLayout());
+		
+		int j = 25;
+        for(int i =0;i<10;i++){
+            JButton button = new JButton("Party " + i);
+            JLabel label = new JLabel("Host Name ");
 
+            mainPanel.add(button);
+            mainPanel.add(label);
+            layout.putConstraint(SpringLayout.WEST, button, 10, SpringLayout.WEST, bottomPanel);
+            layout.putConstraint(SpringLayout.NORTH, button, j, SpringLayout.NORTH, bottomPanel);
+            layout.putConstraint(SpringLayout.NORTH, label, j, SpringLayout.NORTH, bottomPanel);
+            layout.putConstraint(SpringLayout.WEST, label, 20, SpringLayout.EAST, button);
+            j+=30;
+        }
+        mainPanel.setPreferredSize(new Dimension(mainPanel.getWidth(), mainPanel.getHeight()));
+        scroll.setPreferredSize(new Dimension(500,500));
+        scroll.setViewportView(mainPanel);
+        
+        bottomPanel.add(scroll);
 		return bottomPanel;
 	}
 	
