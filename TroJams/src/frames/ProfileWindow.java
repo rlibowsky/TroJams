59b758a376c0017642517c97faae820a88949dc0
Catherine Chung <cathercc@usc.edu>
2016-11-16 01:21:49 -0800

    added buttons/layout to selectionwindow

--- TroJams/src/frames/ProfileWindow.java
+++ TroJams/src/frames/ProfileWindow.java
@@ -1,5 +1,30 @@
 package frames;
 
-public class ProfileWindow {
+import javax.swing.JFrame;
 
+import logic.User;
+
+public class ProfileWindow extends JFrame {
+
+	private User user;
+	
+	public ProfileWindow(User user) {
+		this.user = user;
+		
+		initializeComponents();
+		createGUI();
+		addListeners();
+	}
+	
+	private void initializeComponents() {
+		
+	}
+	
+	private void createGUI() {
+		
+	}
+	
+	private void addListeners() {
+		
+	}
 }
--- TroJams/src/frames/SelectionWindow.java
+++ TroJams/src/frames/SelectionWindow.java
@@ -1,19 +1,46 @@
 package frames;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
 /*
  * SELECTION WINDOW: THIS IS OUR MAIN WINDOW, WHERE WE CHOOSE WHICH PARTY TO JOIN/CREATE A PARTY
  */
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import logic.User;
+import resources.AppearanceConstants;
+import resources.AppearanceSettings;
 
 public class SelectionWindow extends JFrame{
 
+	private JPanel mainPanel;
 	
-	
+	private User user;
+	private JMenuBar menuBar;
+	private JMenu profile;
+	private JMenu logout;
+	private ImageIcon backgroundImage;
+	private JButton createAPartyButton;
 	
 	public SelectionWindow(User user){
+		super("TroJams");
+		this.user = user;
+		
 		initializeComponents();
 		createGUI();
 		addActionListeners();
@@ -22,14 +49,98 @@ public class SelectionWindow extends JFrame{
 
 	private void initializeComponents(){
 		
+		this.setContentPane(new JPanel() {
+			public void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				Image image = new ImageIcon("images/backgroundImage.png").getImage();
+				backgroundImage = new ImageIcon(image.getScaledInstance(1280, 800, java.awt.Image.SCALE_SMOOTH));
+				g.drawImage(image, 0, 0, 1280, 800, this);
+			}
+		});  	
+		mainPanel = new JPanel();
+		createAPartyButton = new JButton("Create a Party");
+		profile = new JMenu("Profile");
+		logout = new JMenu("Logout");
+		menuBar = new JMenuBar();
 	}
 	
 	private void createGUI(){
+		this.setSize(AppearanceConstants.GUI_WIDTH, AppearanceConstants.GUI_HEIGHT);
 		
+		createMenu();
+		createMainPanel();
+		
+		add(mainPanel, BorderLayout.CENTER);
+
+	}
+	
+	// creates the JMenuBar
+	private void createMenu() {
+		AppearanceSettings.setFont(AppearanceConstants.fontSmall, profile, logout);
+		menuBar.add(profile);
+		menuBar.add(logout);
+		setJMenuBar(menuBar);
+	}
+	
+	// creates the top panel, which houses the Create a Party button
+	private JPanel createTopPanel() {
+		JPanel topPanel = new JPanel();
+		
+		topPanel.setSize(AppearanceConstants.GUI_WIDTH, (AppearanceConstants.GUI_HEIGHT)/4);
+		//topPanel.setSize(1280, 200);
+
+		//topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
+		AppearanceSettings.setFont(AppearanceConstants.fontMedium, createAPartyButton);
+		//AppearanceSettings.addGlue(topPanel, BoxLayout.PAGE_AXIS, false, createAPartyButton);
+		topPanel.add(createAPartyButton);
+		topPanel.add(Box.createHorizontalGlue());
+		return topPanel;
+	}
+	
+	// creates the bottom panel, which houses a jscrollpane
+	private JPanel createBottomPanel() {
+		JPanel bottomPanel = new JPanel();
+		JScrollPane songsScrollPane = new JScrollPane();
+		Container contentPane = this.getContentPane();
+
+		return bottomPanel;
+	}
+	
+	// creates the main panel
+	private void createMainPanel() {
+		//mainPanel.setSize(AppearanceConstants.GUI_WIDTH, (AppearanceConstants.GUI_HEIGHT)*(3/4));
+		mainPanel.setSize(1280, 800);
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+		// getting the panel that holds the "create a party" button
+		JPanel topPanel = createTopPanel();
+		// getting the panel that holds the scroll pane with parties
+		JPanel bottomPanel = createBottomPanel();
+		
+		mainPanel.add(topPanel);
+		mainPanel.add(bottomPanel);
 	}
 	
 	private void addActionListeners(){
 		
+		profile.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new ProfileWindow(user);
+			}
+		});
+		
+		logout.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		
+	}
+	
+	public static void main(String [] args) {
+		User user = new User("username", "password");
+		new SelectionWindow(user).setVisible(true);
 	}
 	
 }
