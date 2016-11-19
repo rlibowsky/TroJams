f8bf1ce7bfcaada9a5ca330a211443d9a5615234
Hunter Hurja <HunterHurja@Hunters-MacBook-Pro-2.local>
2016-11-18 18:27:46 -0800

    some refactoring

--- TroJams/src/Test/MusicPlayerTest.java
+++ TroJams/src/Test/MusicPlayerTest.java
@@ -1,32 +1,50 @@
-package test;
+package Test;
 
 import java.io.FileInputStream;
 
+import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
 
-public class MusicPlayerTest {
+public class MusicPlayerTest extends Thread{
 
 	
 	// retrieve text from search, check if vector contains that song
 	// call method with that song name
+	Player playMP3;
 	public MusicPlayerTest(){
 	    try{
 		    //FileInputStream fis = new FileInputStream("music/sunset_lover.mp3");
 	    	FileInputStream fis = new FileInputStream("song_bin/curr_song.mp3");
-		    Player playMP3 = new Player(fis);
-		    playMP3.play();
-		    while (!playMP3.isComplete()) {
-		    	
-		    }
+		    playMP3 = new Player(fis);
+		    this.start();
+		    //playMP3.play();
+		    //playMP3.
+		    Thread.sleep(5000);
+		    System.out.println("done sleeping");
+		    playMP3.close();
+//		    while (!playMP3.isComplete()) {
+//		    	
+//		    }
 		    System.out.println("finished");
-		    FileInputStream fis2 = new FileInputStream("music/kid.mp3");
-		    Player playMP3_2 = new Player(fis2);
-		    playMP3_2.play();
+//		    FileInputStream fis2 = new FileInputStream("music/closer.mp3");
+//		    Player playMP3_2 = new Player(fis2);
+//		    playMP3_2.play();
 		    
 	    }catch(Exception e){
 	    	System.out.println(e);
 	    }
 	}
+	
+	public void run(){
+		try {
+			playMP3.play();
+			//playMP3.
+		} catch (JavaLayerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args){
 		MusicPlayerTest m = new MusicPlayerTest();
 	}
--- TroJams/src/test/Test_Server.java
+++ TroJams/src/test/Test_Server.java
@@ -1,4 +1,4 @@
-package test;
+package Test;
 
 import java.io.File;
 import java.io.IOException;
