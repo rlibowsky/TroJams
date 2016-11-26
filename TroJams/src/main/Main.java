bfd4fa521948547638554ec90d17fc9216043480
Hunter Hurja <hurja@usc.edu>
2016-11-25 19:53:40 -0600

    pizza is good

--- TroJams/src/main/Main.java
+++ TroJams/src/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main (String [] args) {
     	System.out.println("jfieowa");
-    	//new TrojamWelcomeWindow(new TrojamClient("107.170.232.140", 1111)).setVisible(true);
-    	new TrojamWelcomeWindow(new TrojamClient("localhost", 6789)).setVisible(true);
+    	new TrojamWelcomeWindow(new TrojamClient("107.170.232.140", 6789)).setVisible(true);
+    	//new TrojamWelcomeWindow(new TrojamClient("localhost", 6789)).setVisible(true);
     }
 }
