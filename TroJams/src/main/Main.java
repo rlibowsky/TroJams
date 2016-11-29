47ca64cbfd3e7b6a68787bab65b9b5c39eff3402
AdamMoffitt <admoffit@usc.edu>
2016-11-28 23:18:36 -0800

    red selection in jlist

--- TroJams/src/main/Main.java
+++ TroJams/src/main/Main.java
@@ -6,15 +6,9 @@ import networking.TrojamClient;
 public class Main {
 
     public static void main (String [] args) {
-    	System.out.println("jfieowa");
-<<<<<<< HEAD
     	new TrojamWelcomeWindow(new TrojamClient("107.170.232.140", 6789));
     	//new TrojamWelcomeWindow(new TrojamClient("localhost", 6789));
     	//new TrojamWelcomeWindow(new TrojamClient("10.121.30.155", 6789));
-=======
-    	//new TrojamWelcomeWindow(new TrojamClient("10.120.47.41", 6789));
-    	//new TrojamWelcomeWindow(new TrojamClient("localhost", 6789));
-    new TrojamWelcomeWindow(new TrojamClient("10.121.30.155", 6789));
->>>>>>> eb572c36aaea60d9312fc15b98b1b2869082fd3c
+
     }
 }
